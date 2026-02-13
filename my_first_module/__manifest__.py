12edb86eeb0359af9f775c6a38b6e0d9b3467354
trong6so1 <143021279+trong6so1@users.noreply.github.com>
2026-02-13 03:14:35 +0000

    Create __manifest__.py

---
+++ my_first_module/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'My First Module',
+    'version': '17.0.1.0.0',
+    'summary': 'My first Odoo 17 module',
+    'author': 'Trong',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
